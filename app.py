27ca47295067be6a4f2ba0f5080ee5a054aab9b0
PrinceTG80 <tgavkhare80@gmail.com>
2022-06-15 09:52:51 +0530

    code added

--- app.py
+++ app.py
@@ -8,21 +8,21 @@ app = flask.Flask(__name__)
 @app.route('/', methods = ['GET'])
 def home():
     try: 
-        # mobile = '9172310389'
-        # token = request.args['token']
+        mobile = '9172310389'
+        token = request.args['token']
 
-        # authy_api = AuthyApiClient(token)
+        authy_api = AuthyApiClient(token)
 
-        # user = authy_api.users.create(
-        #     email='tgavkhare80@gmail.com',
-        #     phone=mobile,
-        #     country_code=91)
+        user = authy_api.users.create(
+            email='tgavkhare80@gmail.com',
+            phone=mobile,
+            country_code=91)
 
-        # if user.ok():
-        #     print(user.id)
-        #     # user.id is the `authy_id` needed for future requestscurl 612340182
-        # else:
-        #     print(user.errors())
-        return "trying"
+        if user.ok():
+            print(user.id)
+            return user.id
+            # user.id is the `authy_id` needed for future requestscurl 612340182
+        else:
+            return user.errors()
     except:
         return  "Hello World"
