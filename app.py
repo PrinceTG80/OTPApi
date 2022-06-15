adcdeaf91070809c28006167f5c1bebfdffd1005
PrinceTG80 <tgavkhare80@gmail.com>
2022-06-15 10:07:15 +0530

    hardcoded

--- app.py
+++ app.py
@@ -2,21 +2,19 @@ import flask
 from flask import request
 from authy.api import AuthyApiClient
 
+app = flask.Flask(__name__)
 token = 'SFGtP2w2kuBf494bSbmUaRCYeAdEcLHi'
 authy_api = AuthyApiClient(token)
-
-app = flask.Flask(__name__)
+mobile = '9172310389'
+user = authy_api.users.create(
+        email='tgavkhare80@gmail.com',
+        phone=mobile,
+        country_code = 91)
 
 @app.route('/', methods = ['GET'])
 def home():
     try: 
-        mobile = '9172310389'
-        user = authy_api.users.create(
-            email='tgavkhare80@gmail.com',
-            phone=mobile,
-            country_code=91)
         if user.ok():
-            print(user.id)
             return user.id
         else:
             return user.errors()
