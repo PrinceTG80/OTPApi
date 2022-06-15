7b71c997d58dd9f11223816779adbebac20c1d12
PrinceTG80 <tgavkhare80@gmail.com>
2022-06-15 10:04:48 +0530

    runtime error

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import flask
 from flask import request
 from authy.api import AuthyApiClient
 
+token = 'SFGtP2w2kuBf494bSbmUaRCYeAdEcLHi'
+authy_api = AuthyApiClient(token)
 
 app = flask.Flask(__name__)
 
@@ -9,15 +11,10 @@ app = flask.Flask(__name__)
 def home():
     try: 
         mobile = '9172310389'
-        token = 'SFGtP2w2kuBf494bSbmUaRCYeAdEcLHi'
-
-        authy_api = AuthyApiClient(token)
-
         user = authy_api.users.create(
             email='tgavkhare80@gmail.com',
             phone=mobile,
             country_code=91)
-
         if user.ok():
             print(user.id)
             return user.id
