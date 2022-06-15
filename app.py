65d165b3e5604c1dc496678e4010caec044e5301
PrinceTG80 <tgavkhare80@gmail.com>
2022-06-15 15:43:19 +0530

    check

--- app.py
+++ app.py
@@ -17,11 +17,8 @@ def home():
         country_code = 91)
         output = str(user.id)
         if user.ok():
-            sms = authy_api.users.request_sms(str(user.id), {'display_name': str(user.id), "details": {
-                    "title": "Purchase History",
-                    "content": "Product: Unobtanium\n\nDate: 2021-07-07\nQuantity: 1000 units\n\nSales rep: Jessie Robinson"
-            }})
-            output = "{'output' : "+ output +"}"
+            sms = authy_api.users.request_sms(str(user.id), {'display_name': str(user.id)})
+            output = '{"output" : '+ output +''}'
             return output
         else:
             return "error"
