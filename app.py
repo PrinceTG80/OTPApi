af5701b452e658dcab55ebe549048e3f3e9c9f64
PrinceTG80 <tgavkhare80@gmail.com>
2022-06-15 14:30:08 +0530

    token private

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from flask import request
 from authy.api import AuthyApiClient
 
 app = flask.Flask(__name__)
-token = 'SFGtP2w2kuBf494bSbmUaRCYeAdEcLHi'
-authy_api = AuthyApiClient(token)
+
 
 @app.route('/', methods = ['GET'])
 def home():
+    token = str(request.args['token'])
+    authy_api = AuthyApiClient(token)
     mobile = str(request.args['mobile'])
     try: 
         user = authy_api.users.create(
@@ -17,9 +18,9 @@ def home():
         output = str(user.id)
         if user.ok():
             sms = authy_api.users.request_sms(str(user.id), {'display_name': str(user.id), "details": {
-         "title": "Purchase History",
-         "content": "Product: Unobtanium\n\nDate: 2021-07-07\nQuantity: 1000 units\n\nSales rep: Jessie Robinson"
-    }})
+                    "title": "Purchase History",
+                    "content": "Product: Unobtanium\n\nDate: 2021-07-07\nQuantity: 1000 units\n\nSales rep: Jessie Robinson"
+            }})
             if sms.ok():
                 output = output + ' success'
             return output
