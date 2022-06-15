26db52852fa79be30e879f1e24939a3ff6675a6f
PrinceTG80 <tgavkhare80@gmail.com>
2022-06-15 10:28:56 +0530

    api working

--- app.py
+++ app.py
@@ -5,16 +5,20 @@ from authy.api import AuthyApiClient
 app = flask.Flask(__name__)
 token = 'SFGtP2w2kuBf494bSbmUaRCYeAdEcLHi'
 authy_api = AuthyApiClient(token)
-mobile = '9172310389'
-user = authy_api.users.create(
-        email='tgavkhare80@gmail.com',
-        phone=mobile,
-        country_code = 91)
-output = str(user.id)
+
 @app.route('/', methods = ['GET'])
 def home():
     try: 
+        mobile = '9172310389'
+        user = authy_api.users.create(
+        email='tgavkhare80@gmail.com',
+        phone=mobile,
+        country_code = 91)
+        output = str(user.id)
         if user.ok():
+            sms = authy_api.users.request_sms(str(user.id))
+            if sms.ok():
+                output = str(sms.content)
             return output
         else:
             return "error"
